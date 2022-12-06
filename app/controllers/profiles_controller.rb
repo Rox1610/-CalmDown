7b70657ac5edfcf88537d0342bce3f10f2e37122
Roxanne Devillers <roxannedevillers@gmail.com>
2022-12-06 14:10:38 -0500

    ongoing

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,6 @@
+class ProfilesController < ApplicationController
+
+  def index
+    @events = Event.where(user_id: current_user.id)
+  end
+end
---
+++ app/models/event.rb
@@ -0,0 +1,4 @@
+class Event < ApplicationRecord
+  belongs_to :user
+  validates :date, presence: true
+end
