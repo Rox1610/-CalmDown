0ae8fc722c72fd4b1b80b62f21229f440c4da7b7
David Mboma <david.mboma.dm@gmail.com>
2022-12-08 17:48:53 -0500

    fixed some bugs and duplicates

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,12 +11,12 @@ class ProfilesController < ApplicationController
       count_visuel += 1 if params[:mountains] == 1
       count_visuel += 1 if params[:stars] == 1
       count_visuel += 1 if params[:clouds] == 1
-      count_visuel += 1 if params[:flute] == 1
-      count_visuel += 1 if params[:rain] == 1
-      count_visuel += 1 if params[:whale] == 1
-      count_visuel += 1 if params[:mantra] == 1
-      count_visuel += 1 if params[:forest] == 1
-      count_visuel += 1 if params[:mantra] == 1
+      count_auditif += 1 if params[:flute] == 1
+      count_auditif += 1 if params[:rain] == 1
+      count_auditif += 1 if params[:whale] == 1
+      count_auditif += 1 if params[:mantra] == 1
+      count_auditif += 1 if params[:forest] == 1
+      count_auditif += 1 if params[:mantra] == 1
     if count_auditif > count_visuel
       profile = :auditif
     else
@@ -24,7 +24,7 @@ class ProfilesController < ApplicationController
     end
     current_user.profile = Profile.find_by(name: profile)
     if current_user.save
-      redirect_to dashboard_path
+      redirect_to calendar_path
     else
       redirect_to profile_new
     end
