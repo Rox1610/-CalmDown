7156ec8f74e41e18df1d1cc1166d86302f006813
Roxanne Devillers <roxannedevillers@gmail.com>
2022-12-08 17:29:01 -0500

    fix navbar link

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,4 +5,7 @@ class DashboardsController < ApplicationController
     # For a monthly view:
     @events = Event.where(user_id: current_user.id, start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
+
+  def index
+  end
 end
