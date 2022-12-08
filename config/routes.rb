976539fc13801a0f019e56f2b396281115ad40a5
David Mboma <david.mboma.dm@gmail.com>
2022-12-08 14:30:35 -0500

    Solved ongoing conflict, need to bundle install the gems

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :blogs, except: %i[edit update delete]
-<<<<<<< HEAD
   get "home", to: "pages#home"
   resources :profiles, only: %i[new create]
   get "test", to: "pages#test"
-=======
 
   resources :events, except: %i[index] do
     resources :diaries, only: %i[new create]
   end
 
   resources :resources, only: %i[index show]
->>>>>>> d52236c6d4792ad0f54af152a3bb808474b0ff05
 end
