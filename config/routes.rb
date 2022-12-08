90e9635f267c5ff98c3d80d79d3341857be1f30e
Roxanne Devillers <roxannedevillers@gmail.com>
2022-12-08 17:02:12 -0500

    calendar path updated

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get '/kinesthetic', to: 'pages#kinesthetic', as: 'kinesthetic'
-  get '/dashboard', to: 'dashboards#index', as: 'dashboard'
+  get '/dashboard/calendar', to: 'dashboards#calendar', as: 'calendar'
   # Defines the root path route ("/")
   # root "articles#index"
   resources :blogs, except: %i[edit update delete]
